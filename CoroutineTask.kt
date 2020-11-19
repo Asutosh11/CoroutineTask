e26e903a6883b6e4457ab9bcf76e8d7bf9e64955
Asutosh Panda <panda.asutosh04@gmail.com>
2020-11-19 21:03:51 +0530

    for cancel, cancel the job, not the entire scope

--- CoroutineTask.kt
+++ CoroutineTask.kt
@@ -7,7 +7,7 @@ abstract class CoroutineTask<Params, Progress, Result> {
 
     /* AsyncTask has been deprecated from Android 11
      *
-     * CoroutineTask is a utility Class, if you want to seamlessly update your AsyncTasks to Coroutines.
+     * CoroutineTask is a Utility class, if you want to seamlessly update your AsyncTasks to Coroutines.
      * You don't need to change anything in your AsyncTask methods, parameters or logic.
      * It Doesn't matter if your classes are in Java or Kotlin.
      */
@@ -48,13 +48,11 @@ abstract class CoroutineTask<Params, Progress, Result> {
     }
 
     open fun cancel(hasToCancel: Boolean){
-        if(uiScope.isActive && hasToCancel){
-            uiScope.cancel("coroutine cancelled by user")
-        }
-        if(backgroundScope.isActive && hasToCancel){
+        if(hasToCancel){
+            preExecuteJob?.cancel("coroutine cancelled by user")
+            postExecuteJob?.cancel("coroutine cancelled by user")
             doInBackgroundJob?.cancel("coroutine cancelled by user")
+            onCancelled()
         }
-        onCancelled()
     }
 }
-
